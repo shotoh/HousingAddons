d0a0a40c20fedbab41c842d8bf09b8344d80d3a3
shotoh <shotoh0806@gmail.com>
2022-06-07 23:03:15 -0700

    apples

--- src/main/java/io/github/shotoh/housingaddons/features/AutoPrestige.java
+++ src/main/java/io/github/shotoh/housingaddons/features/AutoPrestige.java
@@ -2,12 +2,21 @@ package io.github.shotoh.housingaddons.features;
 
 import io.github.shotoh.housingaddons.utils.Utils;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class AutoPrestige {
-    public static long cooldown = 0;
+    private static final ScheduledExecutorService pool = Executors.newSingleThreadScheduledExecutor();
+    private static long cooldown = 0;
 
     public static void check() {
         if ((System.currentTimeMillis() - cooldown) / 1000.0 < 10) {
-            Utils.sendCommand("/prestige");
+            double delay = Math.random() * 9;
+            pool.schedule(() -> {
+                Utils.sendCommand("/prestige");
+                cooldown = System.currentTimeMillis();
+            }, (long) delay, TimeUnit.SECONDS);
         }
     }
 }
