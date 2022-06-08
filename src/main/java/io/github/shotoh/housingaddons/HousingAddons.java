dc35116eda130362ff900320b6144627fb18ce15
shotoh <shotoh0806@gmail.com>
2022-06-07 23:04:41 -0700

    apples

--- src/main/java/io/github/shotoh/housingaddons/HousingAddons.java
+++ src/main/java/io/github/shotoh/housingaddons/HousingAddons.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class HousingAddons {
     public static final String MODID = "housingaddons";
     public static final String NAME = "HousingAddons";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static Config config;
 
